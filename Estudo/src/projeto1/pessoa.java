294050bf2a04132e04da57f81c365a017bd0f6cd
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 <babro@192.168.15.8>
2025-05-15 19:23:59 -0300

    Operações lógicas aninhadas

--- Estudo/src/projeto1/pessoa.java
+++ Estudo/src/projeto1/pessoa.java
@@ -4,10 +4,27 @@ public class pessoa {
 	
 	public static void main(String[] args) {
 		
-		int numero;
+		int nota1 = 90;
+		int nota2 = 0;
+		int nota3 = 95;		
+		int nota4 = 0;
+		int media = 0;
 		
-		System.out.println("Em nome de jesus");
-
+		media = (nota1+nota2+nota3+nota4) / 4;
+		
+		if (media >= 30) {	
+			if (media >=50) {
+				if(media >70){
+				System.out.println("Aluno aprovado com a nota " + media);
+				}
+			}else {
+				System.out.println("Aluno esta reprovado com a nota " + media);
+			}
+		}
+		
+		
+		
+		
 }
 
 }
