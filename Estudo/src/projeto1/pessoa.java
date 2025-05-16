f3b16d1a3267e8072b7da2751589ed0f296ec9db
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 <babro@192.168.15.8>
2025-05-16 19:59:22 -0300

    Aula operadores relacionais

--- Estudo/src/projeto1/pessoa.java
+++ Estudo/src/projeto1/pessoa.java
@@ -11,38 +11,13 @@ public class pessoa {
 		int media = 0;
 		int dia = 5;
 		
-		media = (nota1+nota2+nota3+nota4) / 4;
+		if ( nota1 > nota2) {
+			System.out.println("maior que o outro");
+		}else {
+			System.out.println("menor que o outro");
+		}
 		
 		
-		switch (dia) {
-		case 1: 
-			System.out.println("Segunda");
-			break;
-		case 2: 
-			System.out.println("Terça");
-			break;
-		case 3: 
-			System.out.println("Quarta");
-			break;
-		case 4: 
-			System.out.println("Quinta");
-			break;
-		case 5: 
-			System.out.println("Sexta");
-			break;
-		case 6: 
-			System.out.println("Sabado");
-			break;
-		case 7: 
-			System.out.println("Domingo");
-			break;
-			
-		
-		
-		default: System.out.println(" Não encontrado");
-			break;
-		
-}
 	}
 
 }
