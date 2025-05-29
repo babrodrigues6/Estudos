1d1b8029d70839f7cfd6fee323a9520c30aaf07c
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 <babro@192.168.15.8>
2025-05-29 19:16:29 -0300

    Criando entrada de dados aula

--- Estudo/src/module-info.java
+++ Estudo/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Estudo {
+	requires java.desktop;
 }
--- Estudo/src/projeto1/pessoa.java
+++ Estudo/src/projeto1/pessoa.java
@@ -1,13 +1,40 @@
 package projeto1;
 
+import javax.swing.JOptionPane;
+
 public class pessoa {
 	
 	public static void main(String[] args) {
 		
-		for (int numero = 0 ; numero <= 10; numero++) {
-			System.out.println("O numero atual é: " + numero);
-		}
 		
+		String banana = JOptionPane.showInputDialog("Quantas bananas são");
+		String pessoa = JOptionPane.showInputDialog("E são quantas pessoas?");
+		
+		double bananaNumero = Double.parseDouble(banana);
+		double pessoaNumero = Double.parseDouble(pessoa);
+		
+		int divisao = (int)(bananaNumero / pessoaNumero); 
+		
+		double resto = bananaNumero % pessoaNumero;
+		
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão");
+		
+		if( resposta == 0) {
+			JOptionPane.showMessageDialog(null, "A quantidade de banana para cada ficou de " + divisao);
+		
+	}else {
+		JOptionPane.showMessageDialog(null, "Então ta meu amigo");
+	}
+		
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o quanto sobrou da operação?");
+		
+		if(resposta == 0){
+			JOptionPane.showMessageDialog(null, "Oque sobrou da divisão foi" + resto);
+		}else
+			JOptionPane.showMessageDialog(null, "Ta blz");
+		
+		
+	
 	}
 
 }
